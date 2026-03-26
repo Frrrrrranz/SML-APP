81de68ee6e722f0809411a5df12ce63024b968b6
35221 <Frrrrrranz@users.noreply.github.com>
2026-03-26 17:49:08 +0800

    优化安卓端页面边界滚动并发布 OTA 1.1.2.1

--- android/app/src/main/java/com/sml/sheetmusic/MainActivity.java
+++ android/app/src/main/java/com/sml/sheetmusic/MainActivity.java
@@ -1,5 +1,17 @@
 package com.sml.sheetmusic;
 
+import android.os.Bundle;
+import android.view.View;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (bridge != null && bridge.getWebView() != null) {
+            bridge.getWebView().setOverScrollMode(View.OVER_SCROLL_NEVER);
+        }
+    }
+}
