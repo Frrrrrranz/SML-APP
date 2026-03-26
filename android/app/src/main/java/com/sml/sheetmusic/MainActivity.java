fc352bcd84ca2bb9b25b96fde92230d14f8683eb
35221 <Frrrrrranz@users.noreply.github.com>
2026-03-26 18:52:52 +0800

    fix: 禁用安卓 WebView overscroll 拉伸，固定底部导航栏

--- android/app/src/main/java/com/sml/sheetmusic/MainActivity.java
+++ android/app/src/main/java/com/sml/sheetmusic/MainActivity.java
@@ -1,5 +1,17 @@
 package com.sml.sheetmusic;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // NOTE: 禁用 WebView 原生 overscroll 拉伸动画
+        // 避免底部导航栏在 overscroll 时跟随整个 WebView 一起形变
+        WebView webView = getBridge().getWebView();
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+    }
+}
